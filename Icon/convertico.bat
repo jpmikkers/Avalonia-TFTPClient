571d4fa548801c72a16f47284aca3f8147d70815
jpmikkers <jean.paul.mikkers@gmail.com>
2025-03-20 14:27:19 +0100

    Add custom app icon

---
+++ Icon/convertico.bat
@@ -0,0 +1,2 @@
+.\imagemagick\magick.exe -density 384 -background none -define icon:auto-resize .\tftpclient.svg .\tftpclient.ico
+.\imagemagick\magick.exe -size 256x256 -background none .\tftpclient.svg .\tftpclient_256x256.png
